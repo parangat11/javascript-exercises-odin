207a79999297f403082365bdb061c0577dde6cf9
parangat11 <parangataditya001@gmail.com>
2024-09-30 01:47:52 +0530

    Solved the problem on calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;  	
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let value = array.reduce((target, num) => {
+    return target + num;
+  }, 0);
+  return value;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let value = array.reduce((target, num) => {
+    return target * num;
+  }, 1);
+  return value;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let ans = 1;
+  for(let i = 0; i < exponent; i++) {
+    ans *= base;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let ans = 1;
+  while(num > 0) {
+    ans *= num;
+    num--;
+  }
+  return ans;
 };
 
 // Do not edit below this line
