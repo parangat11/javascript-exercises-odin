243aab54240b37168b95f8271bada4af2e982eac
parangat11 <parangataditya001@gmail.com>
2024-09-30 02:07:31 +0530

    Solved the palindrome problem. Yet to understand replace function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let n = string.length;
+    let newString = "";
+    for(let i = 0; i < n; i++) {
+        if(string[i] !== ' ') {
+            newString += string[i].toLowerCase();
+        }
+    }
+    newString = remove(newString);
+    n = newString.length;
+    for(let i = 0; i < n; i++) {
+        if(newString[i] !== newString[n - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
+
+function remove(str) {
+    return str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+}
 
 // Do not edit below this line
 module.exports = palindromes;
